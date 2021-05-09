8b716fb7a9b9a25297a3fc84f15ae82fa1a760ae
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-05-09 02:38:06 +0100

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Homepage from './pages/homepage/Homepage';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
